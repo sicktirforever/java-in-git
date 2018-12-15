808a49a1d911f7e1103377f9963029ecc95427b4
Andrei <georgescu85@gmail.com>
2018-12-15 17:52:53 +0200

    Verificam daca numarul este par sau nu si apoi il afisam

--- src/first/java/NumarPar.java
+++ src/first/java/NumarPar.java
@@ -10,5 +10,12 @@ public class NumarPar {
         System.out.println("Valoarea lui n este: ");
          int n = sc.nextInt();
         System.out.println("N = " + n);
+        if (n % 2 == 0){
+            System.out.println("Numarul " + n + " este par");
+        }
+            else {
+            System.out.println("Numarul " + n + " nu este par");
+
+        }
     }
 }
