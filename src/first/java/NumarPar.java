4005777ac0cf57ffb09c9838a0c140d0c8a3f816
Andrei <georgescu85@gmail.com>
2018-12-15 17:44:21 +0200

    Introducem un numar de la tastatura pe care apoi in afisam

---
+++ src/first/java/NumarPar.java
@@ -0,0 +1,14 @@
+package first.java;
+
+import java.util.Scanner;
+
+public class NumarPar {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Valoarea lui n este: ");
+         int n = sc.nextInt();
+        System.out.println("N = " + n);
+    }
+}
