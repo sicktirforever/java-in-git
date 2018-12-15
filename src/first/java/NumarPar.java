be546bb225e8b8fdf8436fe32bdc6244d512bf12
Andrei <georgescu85@gmail.com>
2018-12-15 18:07:01 +0200

    Am creat o metoda prin care aflam daca daca numerele dintr-un array sunt pare sau nu

--- src/first/java/NumarPar.java
+++ src/first/java/NumarPar.java
@@ -7,7 +7,7 @@ public class NumarPar {
 
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Valoarea lui n este: ");
+        System.out.print("Valoarea lui n este: ");
          int n = sc.nextInt();
         System.out.println("N = " + n);
         if (n % 2 == 0){
@@ -18,21 +18,36 @@ public class NumarPar {
 
         }
             numarPrim();
+            numerePrime();
+
     }
 
     public static void numarPrim(){
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Valoarea lui n este: ");
-        int n = sc.nextInt();
-        if (n % 2 == 0){
-            System.out.println("Numarul " + n + " este par");
+        System.out.print("Valoarea lui n este: ");
+        int n1 = sc.nextInt();
+        if (n1 % 2 == 0){
+            System.out.println("Numarul " + n1 + " este par");
         }
         else {
-            System.out.println("Numarul " + n + " nu este par");
+            System.out.println("Numarul " + n1 + " nu este par");
 
         }
         return;
+    }
+
+    public static void  numerePrime(){
+        int[] numere = {12,142,65,43,765,21,1,2,56};
+        for (int nrprim : numere)
+            if (nrprim % 2 == 0){
+                System.out.println(nrprim + " este un numar par");
+            }
+                else{
+                System.out.println(nrprim + " nu este un numar par");
+            }
+                return;
+
     }
 
 }
